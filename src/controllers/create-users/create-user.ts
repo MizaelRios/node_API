215d72a7e5979f8f4d7aae13f7931ed5c9c3c089
Mizael Rios <mizael_sti@hotmail.com>
2023-06-17 20:32:24 -0300

    Feat: Add helpers and refactor controllers

--- src/controllers/create-users/create-user.ts
+++ src/controllers/create-users/create-user.ts
@@ -1,4 +1,6 @@
+import { create } from "domain";
 import { User } from "../../models/user";
+import { badRequest, created, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { CreateUserParams, ICreateUserRepository } from "./protocols";
 import validator from "validator";
@@ -6,40 +8,27 @@ import validator from "validator";
 export class CreateUserController implements IController {
     constructor(private readonly createUserRepository: ICreateUserRepository) { }
 
-    async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User>> {
+    async handle(httpRequest: HttpRequest<CreateUserParams>): Promise<HttpResponse<User | string>> {
         try {
-
             const requiredFields = ["firstName", "lastName", "email", "password"];
 
             for (const field of requiredFields) {
                 if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
-                    return {
-                        statusCode: 400,
-                        body: `Field ${field} is required`
-                    };
+                    return badRequest(`Field ${field} is required`);
                 }
             }
 
             const emailIsValid = validator.isEmail(httpRequest.body!.email);
 
             if (!emailIsValid) {
-                return {
-                    statusCode: 400,
-                    body: "E-mail is valid"
-                };
+                return badRequest("E-mail is invalid");
             }
 
             const user = await this.createUserRepository.createUser(httpRequest.body!)
+            return created<User>(user);
 
-            return {
-                statusCode: 201,
-                body: user
-            };
         } catch (error) {
-            return {
-                statusCode: 500,
-                body: "Something went wrong"
-            }
+            return serverError();
         }
     }
 
--- src/controllers/update-user/update-user.ts
+++ src/controllers/update-user/update-user.ts
@@ -1,51 +1,37 @@
 import { User } from "../../models/user";
+import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { IUpdateUserRepository, UpdateUserParams } from "./protocols";
 
 export class UpdateUserController implements IController {
     constructor(private readonly updateUserRepository: IUpdateUserRepository) { }
 
-    async handle(httpRequest: HttpRequest<UpdateUserParams>): Promise<HttpResponse<User>> {
+    async handle(httpRequest: HttpRequest<UpdateUserParams>): Promise<HttpResponse<User | string>> {
         try {
             const id = httpRequest?.params?.id;
             const body = httpRequest?.body;
 
             if (!body) {
-                return {
-                    statusCode: 400,
-                    body: "Missing fields"
-                };
+                return badRequest("Missing fields");
             }
 
             if (!id) {
-                return {
-                    statusCode: 400,
-                    body: "Something went wrong"
-                };
+                return badRequest("Something went wrong");
             }
 
             const allowedFieldsToUpdate: (keyof UpdateUserParams)[] = ["firstName", "lastName", "password"];
             const someFieldIsNotAllowedToUpdate = Object.keys(body).some(key => !allowedFieldsToUpdate.includes(key as keyof UpdateUserParams));
 
             if (someFieldIsNotAllowedToUpdate) {
-                return {
-                    statusCode: 400,
-                    body: "Some received field is not allowed"
-                };
+                return badRequest("Some received field is not allowed");
             }
 
             const user = await this.updateUserRepository.updateUser(id, body);
 
-            return {
-                statusCode: 200,
-                body: user
-            }
+            return ok(user);
 
         } catch (error) {
-            return {
-                statusCode: 500,
-                body: "Something went wrong"
-            };
+            return serverError();
         }
     }
 
